2c84bdc9da6b8f382f4fa740b34d4904a1f1fb32
Mark Anderson <mark@opscode.com>
2015-02-05 10:18:36 -0800

    Add first spec test

--- spec/orgs.rb
+++ spec/orgs.rb
@@ -5,12 +5,36 @@ require 'fixie'
 require 'fixie/config'
 
 RSpec.describe Fixie::Sql::Orgs, "Organizations access" do
+  let (:test_org) { "ponyville"}
+  
   context "Basic access to orgs" do
+    let (:orgs) { Fixie::Sql::Orgs.new }
     it "We find more than one org" do
-      orgs = Fixie::Sql::Orgs.new
-
-      
       expect(orgs.inner.count).to be > 0
     end
+
+    it "We can find an org" do
+      expect(orgs[test_org].name).to eq(test_org)      
+    end
   end
+
+  context "Search accessors work correctly" do
+    let (:orgs) { Fixie::Sql::Orgs.new }
+    let (:the_org) { orgs[test_org] }
+
+    it "We can find an org by name" do
+      expect(orgs.by_name(test_org).all.count).to eq(1)
+      expect(orgs.by_name(test_org).all.first.name).to eq(the_org.name)
+    end
+
+    # TODO: Automatically extract this from the filter by field
+    %w(name, id, full_name, authz_id).each do |accessor|
+      it "We can access an org by #{accessor}" do
+        expect(orgs.by_name(test_org).all.count).to eq(1)
+        expect(orgs.by_name(test_org).all.first.name).to eq(the_org.name)
+      end
+    end
+    
+  end
+    
 end
