9a8990ab2d84498ea823b207bad0682d8ac793bf
Mark Anderson <mark@chef.io>
2015-05-05 20:09:38 -0700

    Add preliminary code for bulk permission fixup

---
+++ lib/fixie/bulk_edit_permissions.rb
@@ -0,0 +1,106 @@
+#
+# Copyright (c) 2015 Chef Software Inc.
+# License :: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Mark Anderson <mark@chef.io>
+#
+require 'sequel'
+
+require 'fixie/config.rb'
+require 'fixie/authz_objects.rb'
+require 'fixie/authz_mapper.rb'
+
+require 'pp'
+
+module Fixie
+  module BulkEditPermissions
+    def self.orgs
+      @orgs ||= Fixie::Sql::Orgs.new
+    end
+    def self.users
+      @users ||= Fixie::Sql::Users.new
+    end
+    def self.assocs
+      @assocs ||= Fixie::Sql::Associations.new
+    end
+    def self.invites
+      invites ||= Fixie::Sql::Invites.new
+    end
+
+    def self.ace_add(list, ace_type, entity)
+      list.each do |item|
+        if item.respond_to?(:ace_add)
+          item.ace_add(ace_type, entity)
+        else
+          puts "item.class is not a native authz type"
+          return
+        end
+      end
+    end
+    def self.ace_delete(list, ace_type, entity)
+      list.each do |item|
+        if item.respond_to?(:ace_delete)
+          item.ace_delete(ace_type, entity)
+        else
+          puts "item.class is not a native authz type"
+          return
+        end
+      end
+    end
+
+    def self.do_all_objects(org)
+      org = orgs[org] if org.is_a?(String)
+
+      containers = org.containers.all(:all)
+      # Maybe we should fix up containers first?
+      # fix up objects in containers
+      containers.each do |container|
+        # TODO Write some tests to validate that this stuff
+        # works, since it depends on a lot of name magic...
+        object_type = container.name.to_sym
+#        raise Exception "No such object_type #{object_type}" unless org.respond_to?(object_type)
+        objects = org.send(object_type).all(:all)
+        if block_given?
+          yield objects
+        end
+      end
+    end
+
+    def self.ace_add_all(org, ace_type, entity)
+      org = orgs[org] if org.is_a?(String)
+      org.each_authz_object_by_class do |objects|
+        ace_add(objects, ace_type, entity)
+      end
+    end
+
+    def self.ace_delete_all(org, ace_type, entity)
+      org = orgs[org] if org.is_a?(String)
+      org.each_authz_object_by_class do |objects|
+        ace_delete(objects, ace_type, entity)
+      end
+    end
+
+    def self.add_admin_permissions(org)
+      # rework when ace add takes multiple items...
+      admins = org.groups['admins']
+      pivotal = users['pivotal']
+      org.each_authz_object_by_class do |objects|
+        ace_add(objects, :all, pivotal)
+        ace_add(objects, :all, admins)
+      end
+    end    
+    
+  end
+end
