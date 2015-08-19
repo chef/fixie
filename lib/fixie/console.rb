1fc7d7895fdfe9534956a5e58ca1b9ba30a9cf71
Jeremiah Snapp <jeremiah@chef.io>
2015-08-19 13:58:38 -0400

    Abstracted how fixie is configured and setup

--- lib/fixie/console.rb
+++ lib/fixie/console.rb
@@ -31,16 +31,17 @@ module Fixie
 
     def start
       configure
-      setup
+      Fixie.setup
       configure_pry
       Pry.start
     end
 
     def configure
+      config_file = nil
       if ARGV.first && ARGV[0].chars.first != "-" && config_file = ARGV.shift
         config_file = File.expand_path(config_file)
-        load_config_file = true
       end
+      Fixie.load_config(config_file)
 
       options = {}
       OptionParser.new do |opt|
@@ -58,29 +59,8 @@ module Fixie
       end
       pp :cli_opts => options if ENV["DEBUG"]
 
-      if load_config_file
-        puts "loading config: #{config_file}..."
-        Kernel.load(config_file)
-      else
-        path = "/etc/opscode"
-        puts "loading config from #{path}"
-        Fixie::Config.instance.load_from_pc(path)
-      end
-
       Fixie::Config.instance.merge_opts(options)
       puts Fixie::Config.instance.to_text
-    end
-
-    def setup
-      # TODO: do we have to polute global object with this to make it available to the irb instance?
-      Object.const_set(:ORGS, Fixie::Sql::Orgs.new)
-      Object.const_set(:USERS, Fixie::Sql::Users.new)
-      Object.const_set(:ASSOCS, Fixie::Sql::Associations.new)
-      Object.const_set(:INVITES, Fixie::Sql::Invites.new)
-
-      # scope this by the global org id?
-      Object.const_set(:GLOBAL_GROUPS, Fixie::Sql::Groups.new.by_org_id(Fixie::Sql::Orgs::GlobalOrg))
-      Object.const_set(:GLOBAL_CONTAINERS, Fixie::Sql::Containers.new.by_org_id(Fixie::Sql::Orgs::GlobalOrg))
     end
 
     def configure_pry
